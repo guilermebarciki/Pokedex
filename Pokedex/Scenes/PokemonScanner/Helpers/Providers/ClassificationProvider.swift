e8360a97e3eeffdabf6574dd473bf8de2fed163a
Guilerme Barciki <guilermebarciki@MacBook-Pro-de-fantauva.local>
2024-08-01 08:06:34 -0300

    create more protocols for pokemon scanner responsabilities

---
+++ Pokedex/Scenes/PokemonScanner/Helpers/Providers/ClassificationProvider.swift
@@ -0,0 +1,8 @@
+//
+//  ClassificationProvider.swift
+//  Pokedex
+//
+//  Created by Guilerme Barciki   on 01/08/24.
+//
+
+import Foundation
---
+++ Pokedex/Scenes/PokemonScanner/Helpers/Providers/ImageProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  ImageProcessor.swift
+//  Pokedex
+//
+//  Created by Guilerme Barciki   on 01/08/24.
+//
+
+import Foundation
